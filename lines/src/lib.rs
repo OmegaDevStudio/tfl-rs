e5bc6996f209d912c05b731071ebb16968ceabad
Shell1010 <amin.dev03@gmail.com>
2024-01-20 23:09:36 +0000

    :rage: im killing myself

--- lines/src/lib.rs
+++ lines/src/lib.rs
@@ -7,6 +7,7 @@ mod tests {
     use crate::{client::Client, datastructs::DataStruct};
     #[test]
     fn search_test() {
+        // Does test for search/query
         let resp = Client::new("abcd1234").query("205").fetch().unwrap();
         if let DataStruct::QuerySearch(data) = resp {
             assert!("Tfl.Api.Presentation.Entities.RouteSearchResponse, Tfl.Api.Presentation.Entities" == data.type_field, "Type Returned: {}", data.type_field)
